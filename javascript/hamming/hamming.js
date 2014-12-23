d60b4c587a207df31a8ff30e9a6dbe89ee12aaea
martinhb <martin.bielecki@gmail.com>
2014-12-24 00:23:19 +0100

    Exercise 3: hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,4 +1,30 @@
-function compute (seq1, seq2) {
-    // body...
+function compute(seq1, seq2) {
+    if(!sequencesHaveEqualLength(seq1, seq2)) {
+        throw new Error('DNA strands must be of equal length.');
+    }
+
+    return calculateHammingDistance(seq1, seq2, 0);
 }
 
+function calculateHammingDistance(seq1, seq2, sum) {
+    if (!seq1) {
+        return sum;
+    }
+
+    var seqTail1 = seq1.slice(1),
+        seqTail2 = seq2.slice(1);
+
+    return equalCharacter(seq1, seq2) ?
+        calculateHammingDistance(seqTail1, seqTail2, sum) :
+        calculateHammingDistance(seqTail1, seqTail2, ++sum);
+
+    function equalCharacter(seq1, seq2) {
+        return seq1[0] === seq2[0];
+    }
+}
+
+function sequencesHaveEqualLength(seq1, seq2) {
+    return seq1.length === seq2.length;
+}
+
+module.exports.compute = compute;
