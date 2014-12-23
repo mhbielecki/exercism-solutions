c7bd5cdbf46df970a98b787423394163f70bc526
martinhb <martin.bielecki@gmail.com>
2014-12-23 22:44:22 +0100

    space indendation

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,33 +2,33 @@ var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
 
-	input = input.trim();
+    input = input.trim();
 
-	if (isYelling(input)) {
-		return 'Whoa, chill out!';
-	}
-	else if (isQuestion(input)) {
-		return 'Sure.';
-	}
-	else if (isSilent(input)) {
-		return 'Fine. Be that way!';
-	}
-	else {
-		return 'Whatever.';
-	}
+    if (isYelling(input)) {
+        return 'Whoa, chill out!';
+    }
+    else if (isQuestion(input)) {
+        return 'Sure.';
+    }
+    else if (isSilent(input)) {
+        return 'Fine. Be that way!';
+    }
+    else {
+        return 'Whatever.';
+    }
 };
 
 function isYelling(input) {
-	return input.toUpperCase() === input 
-		&& input.match('[a-zA-z]+');
+    return input.toUpperCase() === input 
+        && input.match('[a-zA-z]+');
 }
 
 function isQuestion(input) {
-	return input.slice(-1) === '?';
+    return input.slice(-1) === '?';
 }
 
 function isSilent(input) {
-	return input === '';
+    return input === '';
 }
 
 module.exports = Bob;
--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,21 +1,21 @@
 var words = function (input) {
-	var wordDictionary = {};
-	var wordList = input.split(/\s+/);
-	wordList.map(addWordToDict);
-	return wordDictionary;
+    var wordDictionary = {};
+    var wordList = input.split(/\s+/);
+    wordList.map(addWordToDict);
+    return wordDictionary;
 
-	function addWordToDict(word) {
-		if (wordExists(wordDictionary[word])) {
-			wordDictionary[word] = ++wordDictionary[word];
-		}
-		else {
-			wordDictionary[word] = 1;
-		}
-	}
+    function addWordToDict(word) {
+        if (wordExists(wordDictionary[word])) {
+            wordDictionary[word] = ++wordDictionary[word];
+        }
+        else {
+            wordDictionary[word] = 1;
+        }
+    }
 }
 
 function wordExists(num) {
-	return !isNaN(num);
+    return !isNaN(num);
 }
 
 module.exports = words;
