edfc5b8182a8c57eeddd913bd2262d556b5f7e50
Martin H. Bielecki <martin.bielecki@gmail.com>
2015-03-03 22:42:46 +0100

    F# sum-of-multiples

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,13 @@
+﻿module public SumOfMultiples
+
+type SumOfMultiples(coefficients: List<int>) = 
+
+    let multipleOfCoefficients num = 
+        Seq.exists (fun coefficient -> num % coefficient = 0) coefficients
+
+    new() = SumOfMultiples([3; 5])
+
+    member this.To(x: int) = 
+        seq {1 .. x-1} 
+        |> Seq.filter(multipleOfCoefficients)
+        |> Seq.sum
---
+++ fsharp/sum-of-multiples/SumOfMultiplesTest.fs
@@ -0,0 +1,36 @@
+﻿module SumOfMultiplesTest
+
+open NUnit.Framework
+open SumOfMultiples
+
+[<TestFixture>]
+type SumOfMultiplesTest() =
+    let mutable sumOfMultiples = SumOfMultiples()
+    
+    [<Test>]
+    member tc.Sum_to_1() = 
+        Assert.That(sumOfMultiples.To(0), Is.EqualTo(0))
+
+    [<Test>]
+    member tc.Sum_to_3() = 
+        Assert.That(sumOfMultiples.To(3), Is.EqualTo(0))
+
+    [<Test>]
+    member tc.Sum_to_10() = 
+        Assert.That(sumOfMultiples.To(10), Is.EqualTo(23))
+
+    [<Test>]
+    member tc.Sum_to_15() = 
+        Assert.That(sumOfMultiples.To(15), Is.EqualTo(45))
+
+    [<Test>]
+    member tc.Sum_to_1000() =
+        Assert.That(sumOfMultiples.To(1000), Is.EqualTo(233168))
+
+    [<Test>]
+    member tc.Configurable_7_13_17_to_20() = 
+        Assert.That(SumOfMultiples([7; 13; 17]).To(20), Is.EqualTo(51))
+
+    [<Test>]
+    member tc.Configurable_43_47_to_10000() = 
+        Assert.That(SumOfMultiples([43; 47]).To(10000), Is.EqualTo(2203160))
