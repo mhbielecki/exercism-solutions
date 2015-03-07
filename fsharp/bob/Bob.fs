1a839a5b4fd89d68d76ae1679794136f649362fe
Martin H. Bielecki <martin.bielecki@gmail.com>
2015-03-07 19:45:42 +0100

    FSharp. Exercise 3, iteration 1: bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,17 @@
+﻿module Bob
+open System
+open System.Text.RegularExpressions
+
+type Bob(sentence:string) = 
+
+    let isYelling() = 
+        String.Compare(sentence.ToUpper(), sentence) = 0 
+            && Regex.IsMatch(sentence, @"[A-Za-z]")
+    let isQuestion() = sentence.EndsWith("?")
+    let isSilent() = String.IsNullOrEmpty(sentence.Trim())
+
+    member this.hey() =
+        if isYelling() then "Whoa, chill out!"
+        elif isQuestion() then "Sure."
+        elif isSilent() then "Fine. Be that way!"
+        else "Whatever."
