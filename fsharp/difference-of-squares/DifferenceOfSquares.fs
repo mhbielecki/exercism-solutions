0f438a8ea6397e5df159cf0afb3df19d30c81eef
Martin H. Bielecki <martin.bielecki@gmail.com>
2015-03-07 13:13:13 +0100

    Exercise 2, iteration 1: difference-of-squares

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,15 @@
+﻿module DifferenceOfSquares
+
+type DifferenceOfSquares(n: int) =
+
+    let square x = x * x
+
+    member this.squareOfSums() =
+         square(seq{1..n} |> Seq.sum)
+
+    member this.sumOfSquares() =
+        seq {1 .. n}
+        |> Seq.map square 
+        |> Seq.sum
+
+    member this.difference() = this.squareOfSums() - this.sumOfSquares()
